151eadab422dad79510ec5a23b32d55e8797062c
JAGScript <jagandres_guerrero@hotmail.com>
2019-02-09 02:24:10 -0500

    second pull

--- js/modernizr/modernizr-custom.js
+++
@@ -1,4 +0,0 @@
-/* Modernizr (Custom Build) | MIT & BSD
- * Build: http://modernizr.com/download/#-shiv-load-mq-cssclasses-teststyles-testprops-testallprops-prefixes-domprefixes-csstransforms
- */
-;;
